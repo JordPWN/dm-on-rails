4058820f8a149a444d55fa3fce1e044051a6d57b
JordPWN <jordan.ratanadawong@gmail.com>
2016-05-22 21:49:48 +0000

    Removed end from layout

--- app/actions.rb
+++ app/actions.rb
@@ -43,32 +43,8 @@ get '/' do
     @startIndex = @endIndex - (@records_per_page-1)
   end
 
-  erb :index
+  erb :'index'
 end
-
-
-
-# test code for pagination below:
-# puts @total_pages
-# get '/paginate' do 
-#   @campaigns = Campaign.all
-#   @total_pages = ((@campaigns.length.to_f)/@records_per_page.to_f).to_f
-#   @total_pages = @total_pages.ceil
-#   puts "WELCOME"
-#   puts @total_pages
-
-#   if params[:pageid]==nil
-#     @current_page = 1
-#     @endIndex = (@current_page * @records_per_page)-1 # because of the index that starts with 0
-#     @startIndex = @endIndex - @records_per_page
-#   else
-#     @current_page = params[:pageid].to_i
-#     @endIndex = (@current_page * @records_per_page)-1 # because of the index that starts with 0
-#     @startIndex = @endIndex - (@records_per_page-1)
-#   end
-
-#   erb :mypage
-# end
 
 
 
