ca4b9174c4c2e71da3c2cffb99eb692a2ab738fd
JordPWN <jordan.ratanadawong@gmail.com>
2016-05-23 01:31:41 +0000

    Fucking with tables

--- db/migrate/20160522195012_delete_ratings_table.rb
+++
@@ -1,5 +0,0 @@
-class DeleteRatingsTable < ActiveRecord::Migration
-  def change
-    drop_table :ratings
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,3 @@
 require 'faker'
 
-@user = User.create(username: Faker::name, email: Faker::Internet.email, password: Faker::Internet.password)
-
-
-@campaign = Campaign.create(title: Faker::Book.title, user_id: @user.id, description: Faker::Lorem.paragraph, origin: Faker::name, url: Faker::Company.logo )
-
-
-@beat = Beat.create(campaign_id: @campaign.id, content: Faker::Lorem.paragraphs, ordinance: 0)
-Beat.create(campaign_id: @campaign.id, content: Faker::Lorem.paragraphs, ordinance: 1)
-Beat.create(campaign_id: @campaign.id, content: Faker::Lorem.paragraphs, ordinance: 2)
-
-
-@game = Game.create(user_id: @user.id, campaign_id: @campaign.id, beat_id: @beat.id)
-
-
+@user = User.create(username: "GaryGygax", email: Faker::Internet.email, password: Faker::Internet.password)
